550f952597ea5d54addfcf4b506ff6bdb2367920
思亮 孙 <sun418096041@outlook.com>
2021-12-22 23:15:31 +0800

    css,js,accdb

---
+++ src/main/java/JavaBean/User.java
@@ -0,0 +1,25 @@
+package JavaBean;
+
+public class User {
+    private String userName;
+    private String userPassword;
+
+    public User() {
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return this.userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
